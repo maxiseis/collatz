90ebc259d0bfb70a497685985dffe3488eca28d8
maxiseis <leechbenstein@gmail.com>
2026-03-07 22:15:59 +0700

    Fix input prompt and correct Collatz calculations

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-n = int(input("Which Number("x" if forever):"))
+n = int(input("Which Number:"))
 steps = 0
 
 while n != 1:
   if n % 2 == 0:
-    n // 2
+   n = n // 2
   else:
-    n * 3 + 1
+   n = n * 3 + 1
   steps += 1
 print(1, steps)
