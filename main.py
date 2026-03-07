44bd41ad99295a0ec3fcf725296e64c5ea1b14dd
maxiseis <leechbenstein@gmail.com>
2026-03-07 15:23:45 +0000

    fastest

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+cache = {1: 0}  # known sequence lengths
+
 n = 1
+max_steps = 0
 
 while True:
     x = n
-    steps = 0
+    path = []
 
-    while x != 1:
+    while x not in cache:
+        path.append(x)
         if x % 2 == 0:
             x //= 2
         else:
-            x = 3*x + 1
-        steps += 1
+            x = 3 * x + 1
 
-    print(n, steps)
+    steps = cache[x]
+
+    for i, v in enumerate(reversed(path)):
+        steps += 1
+        cache[v] = steps
+
+    if cache[n] > max_steps:
+        max_steps = cache[n]
+        print(f"New record: n={n} steps={max_steps}")
+
     n += 1
